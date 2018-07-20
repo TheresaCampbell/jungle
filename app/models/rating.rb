9b7ca6774639686f60ca4c794146e0272eb5a268
Theresa Campbell <t.campbell@sasktel.net>
2018-07-20 15:28:01 +0000

    Added validations to reviews. Each review must have a corresponding user and product. Reviews must have one (and can have both) of the following: rating, description.

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,15 @@
 class Rating < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
+
+  validates :product_id, presence: true
+  validates :user_id, presence: true
+  validates :has_rating_or_description?
+
+  def has_rating_or_description?
+    if rating == nil || description == nil
+      errors.add(:rating, "needs a rating or a description")
+      errors.add(:description, "needs a rating or a description")
+    end
+  end
 end
