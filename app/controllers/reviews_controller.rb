1b2173d3cc952838f690729e75911f900c75bd7e
Theresa Campbell <t.campbell@sasktel.net>
2018-07-20 21:15:05 +0000

    Users can delete their own reviews, but not others' reviews. Home page shows the overall average rating of each product (if that product has ratings.)

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,20 @@
 class ReviewsController < ApplicationController
 
+before_action :current_user
+
   def create
    @product = Product.find(params[:product_id])
    @review = @product.reviews.create(review_params)
    @review.user = current_user
    @review.save!
    redirect_to product_path(@product)
+ end
+
+ def destroy
+  @product = Product.find(params[:product_id])
+  @review = Review.find params[:id]
+  @review.destroy
+  redirect_to product_path(@product)
  end
 
   private
