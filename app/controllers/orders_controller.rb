dd6514c4d7b16f08e8baab665cad707a5d67f1e5
Theresa Campbell <t.campbell@sasktel.net>
2018-07-21 02:58:57 +0000

    Cleaning up dead code.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,7 +13,7 @@ class OrdersController < ApplicationController
     if order.valid?
       UserMailer.receipt_email(order).deliver_now
       empty_cart!
-      redirect_to order, notice: 'Your Order has been placed.'
+      redirect_to order
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
     end
